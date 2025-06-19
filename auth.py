a1d378ab1a7d28caef979238f9ab9fb8681a72c1
Cenilss <99053907+Cenilss@users.noreply.github.com>
2025-06-19 14:01:24 +0000

    Update Auth

--- auth.py
+++ auth.py
@@ -1,10 +1,7 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import streamlit as st
 
 def check_login(username, password):
     return (
-        username == os.getenv("ADMIN_USER") and
-        password == os.getenv("ADMIN_PASS")
+        username == st.secrets["USERNAME"]
+        and password == st.secrets["PASSWORD"]
     )
